b3f7f8c26be22037f81d871fdf87411ff2e5b7ee
Tarun Jaiswal <tarunjais28@gmail.com>
2025-06-18 23:40:34 +0530

    Colourised

--- scripts/scanAbiFunctions.js
+++ scripts/scanAbiFunctions.js
@@ -1,5 +1,6 @@
 const IUniswapV2Router02 = require("@uniswap/v2-periphery/build/IUniswapV2Router02.json");
 const { ethers } = require("ethers");
+const chalk = require('chalk');
 
 const functionNames = IUniswapV2Router02.abi
   .filter((item) => item.type === "function")
@@ -9,5 +10,5 @@ const iface = new ethers.Interface(IUniswapV2Router02.abi);
 
 for (const name of functionNames) {
   const methodId = iface.getFunction(name).selector;
-  console.log(`${name}: ${methodId}`);
+  console.log(chalk.green(methodId) + ': ' + chalk.green(name));
 }
